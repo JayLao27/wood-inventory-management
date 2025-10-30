d37e661aa69de7cde31a84bfeb5e47d5300b6980
Jay Lao <cjaylao447@gmail.com>
2025-10-30 23:59:56 +0800

    production fix

--- resources/views/Systems/production.blade.php
+++ resources/views/Systems/production.blade.php
@@ -107,7 +107,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
                                 </svg>
                             </div>
-                            <input type="text" placeholder="Search work orders..." class="w-[800px] pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent">
+                            <input type="text" placeholder="Search work orders..." class="w-[850px] pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-transparent">
                         </div>
                     </div>
                     <div class="flex space-x-3">
