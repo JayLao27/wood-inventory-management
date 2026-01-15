a5dc1f64c42e210681b67074f15a54f9e753c311
Jay Lao <cjaylao447@gmail.com>
2026-01-15 08:24:33 +0800

    update: procurement color modal button

--- resources/views/Systems/procurement.blade.php
+++ resources/views/Systems/procurement.blade.php
@@ -122,7 +122,7 @@
                     </button>
                 </div>
                 <div id="suppliersButton" class="flex space-x-3 hidden">
-                    <button onclick="openAddSupplierModal()" class="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition flex items-center space-x-2">
+                    <button onclick="openAddSupplierModal()" class="px-4 py-2 bg-white text-[#374151] rounded-lg hover:bg-[#DEE4EF] transition flex items-center space-x-2">
                         <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd"/>
                         </svg>
