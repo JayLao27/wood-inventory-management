c0c8b4d9f232d733379837467a06addbf0766dd2
Jay Lao <cjaylao447@gmail.com>
2026-01-15 09:13:37 +0800

    modified: logo dashboard

--- resources/views/Systems/dashboard.blade.php
+++ resources/views/Systems/dashboard.blade.php
@@ -62,7 +62,7 @@
 								<p class="text-white text-sm mt-1">Require immediate attention</p>
 							</div>
 							<div >
-								@include('components.icons.alert', ['class' => 'icon-alert'])
+								@include('components.icons.error', ['class' => 'icon-error'])
 							</div>
 						</div>
 					</div>
