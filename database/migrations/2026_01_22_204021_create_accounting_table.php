b80c85f291540002db8cbbf5dc0eae72ed7a4d30
Jay Lao <cjaylao447@gmail.com>
2026-01-23 05:31:13 +0800

    create: database accounting

--- database/migrations/2026_01_22_204021_create_accounting_table.php
+++ database/migrations/2026_01_22_204021_create_accounting_table.php
@@ -6,14 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+ 
     public function up(): void
     {
         Schema::create('accounting', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('account_name');
         });
     }
 
