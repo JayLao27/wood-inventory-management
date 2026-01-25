0b2ddd5adace19c7ef628bed47658070d0129e0f
Jay Lao <cjaylao447@gmail.com>
2026-01-25 12:55:57 +0800

    add: views layout remove the profile intent from nav bar profile

--- resources/views/layouts/system.blade.php
+++ resources/views/layouts/system.blade.php
@@ -1,12 +1,29 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-@section('content')
-<div class="flex h-screen bg-gray-100">
-    @include('layouts.sidebar')
+        <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Main Content -->
-    <div class="flex-1 flex flex-col overflow-hidden">
-        @yield('main-content')
-    </div>
-</div>
-@endsection
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    </head>
+    <body class="font-sans text-gray-900 antialiased">
+        <div class="flex h-screen bg-gray-100">
+            @include('layouts.sidebar')
+
+            <!-- Main Content -->
+            <div class="flex-1 flex flex-col overflow-hidden">
+                @yield('main-content')
+            </div>
+        </div>
+    </body>
+</html>
