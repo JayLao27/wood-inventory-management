2cb0ef7fffc10e1f4e4ccc6ad169a50915c020c4
Jay Lao <cjaylao447@gmail.com>
2026-01-27 21:21:09 +0800

    add/modified: transaction for accounting

--- app/Models/Accounting.php
+++ app/Models/Accounting.php
@@ -4,23 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-use function PHPSTORM_META\type;
-
 class Accounting extends Model
 {
-  
     protected $table = 'accountings';
 
     protected $fillable = [
         'transaction_type',
-        'reference_id',
-        'transaction_number',
-        'type',
         'amount',
+        'date',
         'description',
-        'transaction_date',
-        'payment_method',
+        'sales_order_id',
+        'purchase_order_id',
     ];
-        
 
+    protected $casts = [
+        'date' => 'date',
+        'amount' => 'decimal:2',
+    ];
+
+    // Relationships
+    public function salesOrder()
+    {
+        return $this->belongsTo(SalesOrder::class);
+    }
+
+    public function purchaseOrder()
+    {
+        return $this->belongsTo(PurchaseOrder::class);
+    }
 }
