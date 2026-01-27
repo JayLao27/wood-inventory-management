af443fe3fb31538248bf41df202a88a0fa3ba36a
Jay Lao <cjaylao447@gmail.com>
2026-01-27 13:30:12 +0800

    add: protected models fillable

--- app/Models/Accounting.php
+++ app/Models/Accounting.php
@@ -4,9 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use function PHPSTORM_META\type;
+
 class Accounting extends Model
 {
   
+    protected $table = 'accountings';
+
+    protected $fillable = [
+        'transaction_type',
+        'reference_id',
+        'transaction_number',
+        'type',
+        'amount',
+        'description',
+        'transaction_date',
+        'payment_method',
+    ];
         
 
 }
