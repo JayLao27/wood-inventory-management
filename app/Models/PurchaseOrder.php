357434da3799372a534abef97eb4f99229834979
Jay Lao <cjaylao447@gmail.com>
2026-01-27 21:21:28 +0800

    modified

--- app/Models/PurchaseOrder.php
+++ app/Models/PurchaseOrder.php
@@ -11,7 +11,7 @@ class PurchaseOrder extends Model
     protected $fillable = [
         'order_number',
         'supplier_id',
-        'order_date',
+        'order_date', 
         'expected_delivery',
         'total_amount',
         'paid_amount',
