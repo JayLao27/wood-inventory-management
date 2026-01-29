eee5a4f2cc2df6f8a62c2a9786a05412f3fed1af
Jay Lao <cjaylao447@gmail.com>
2026-01-30 01:13:15 +0800

    feat: add edit button for products in inventory view

--- resources/views/Systems/inventory.blade.php
+++ resources/views/Systems/inventory.blade.php
@@ -217,6 +217,11 @@
                         @endif
 
                         <div class="flex items-center space-x-2 mt-3 justify-end">
+                            <button onclick="event.stopPropagation(); openEditProductModal({{ $product->id }})" class="p-1 hover:bg-slate-500 rounded" title="Edit">
+                                <svg class="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"/>
+                                </svg>
+                            </button>
                             <button onclick="event.stopPropagation(); deleteItem('product', {{ $product->id }})" class="p-1 hover:bg-slate-500 rounded" title="Delete">
                                 <svg class="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
                                     <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l1-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
