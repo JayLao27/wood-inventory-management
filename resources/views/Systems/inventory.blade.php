405a9db1d589dab85cf24dc5bb59d7cea9466e83
Jay Lao <cjaylao447@gmail.com>
2026-01-30 14:01:24 +0800

    feat: update delete button icon for material items in inventory

--- resources/views/Systems/inventory.blade.php
+++ resources/views/Systems/inventory.blade.php
@@ -163,8 +163,7 @@
                                 </button>
                                 <button onclick="event.stopPropagation(); deleteItem('material', {{ $material->id }})" class="p-2 hover:bg-slate-500 rounded" title="Delete">
                                     <svg class="w-5 h-5 text-white" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" clip-rule="evenodd"/>
-                                        <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"/>
+                                        <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"/>
                                     </svg>
                                 </button>
                             </div>
