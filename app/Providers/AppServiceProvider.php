4ddc7f27a7f7a4aa266e1df9abfddb6a2c0541b6
Jay Lao <cjaylao447@gmail.com>
2026-02-10 11:25:23 +0800

    feat: organize and format boot method in AppServiceProvider for improved readability

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Material;
+use App\Models\Product;
+use App\Models\PurchaseOrder;
+use App\Models\WorkOrder;
+use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,9 +22,15 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-   public function boot(): void
-{
+    public function boot(): void
+    {
+        Relation::morphMap([
+            'material' => Material::class,
+            'product' => Product::class,
+            'purchase_order' => PurchaseOrder::class,
+            'work_order' => WorkOrder::class,
+        ]);
 
-    \Illuminate\Support\Facades\Route::redirect('/dashboard', '/sales');
-}
+        \Illuminate\Support\Facades\Route::redirect('/dashboard', '/sales');
+    }
 }
