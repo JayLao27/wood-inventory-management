df5b437b4156fc27cc40d8075faaf617ec86a691
Jay Lao <cjaylao447@gmail.com>
2026-02-20 20:35:33 +0800

    🔧 update (material): add HasFactory trait to Material model

--- app/Models/Material.php
+++ app/Models/Material.php
@@ -7,8 +7,11 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 class Material extends Model
 {
+    use HasFactory;
     protected $fillable = [
         'name',
         'category',
